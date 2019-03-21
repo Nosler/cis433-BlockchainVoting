cf7b5f2aa7ddc75666f360a5f24252ebc1e64683
Nosler <andinosler@gmail.com>
2019-03-20 18:16:09 -0700

    added JS code for retrieving votes

--- static/scriptRES.js
+++ static/scriptRES.js
@@ -1,24 +1,30 @@
 window.onload=function(){
-  var vote_counts = [5, 1, 69, 1, 5];
-  var winners = [false, false, false, false, false];
-  var badges = document.getElementsByClassName("badge");
-  var i;
-  var maxIndex = 0;
-  for(i = 0; i < 5; i++){
-      badges[i].textContent=vote_counts[i];
-      if(vote_counts[i] > vote_counts[maxIndex]){
-          winners = [false, false, false, false, false];
-          maxIndex = i;
-          winners[i] = true;
-      } else if (vote_counts[i] == vote_counts[maxIndex]){
-          winners[i] = true;
-      }
-  }
+  $.get("get_results/", function(data, status){
+       count_votes(data);
+   }, "json");
+}
 
-  for(i = 0; i < 5; i++){
-      if(winners[i] == true){
-          badges[i].classList.remove("badge-info");
-          badges[i].classList.add("badge-success");
-      }
-  }
+function count_votes(dict){
+    var vote_counts = [dict.candidate1, dict.candidate2, dict.candidate3, dict.candidate4, dict.candidate5];
+    var winners = [false, false, false, false, false];
+    var badges = document.getElementsByClassName("badge");
+    var i;
+    var maxIndex = 0;
+    for(i = 0; i < 5; i++){
+        badges[i].textContent=vote_counts[i];
+        if(vote_counts[i] > vote_counts[maxIndex]){
+            winners = [false, false, false, false, false];
+            maxIndex = i;
+            winners[i] = true;
+        } else if (vote_counts[i] == vote_counts[maxIndex]){
+            winners[i] = true;
+        }
+    }
+
+    for(i = 0; i < 5; i++){
+        if(winners[i] == true){
+            badges[i].classList.remove("badge-info");
+            badges[i].classList.add("badge-success");
+        }
+    }
 }
